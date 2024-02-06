09068c4962723e24906b48490a1fd00acf982f0d
RRyapov <rryapov@gmail.com>
2024-02-06 15:41:31 +0300

    First commit to push

---
+++ src/components/Categories.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function Categories({ items }) {
+  const [activeItem, setActiveItem] = useState(null);
+
+  return (
+    <div className="categories">
+      <ul>
+        <li
+          className={activeItem === null ? "active" : ""}
+          onClick={() => setActiveItem(null)}
+        >
+          Все
+        </li>
+        {items &&
+          items.map((name, index) => (
+            <li
+              className={activeItem === index ? "active" : ""}
+              onClick={() => setActiveItem(index)}
+              key={`${name}_${index}`}
+            >
+              {name}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
